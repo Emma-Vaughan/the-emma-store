e67900512cb2f4f58e857ad556df30b877563390
Emma Vaughan <emma.vaughan91@hotmail.com>
2021-04-16 15:22:22 +0100

    Added product component
    Mapping through JSON products for responsive display

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,16 +1,31 @@
-fetch("http://localhost:8000/catalogue")
-  .then((res) => {
-    if (!res.ok) {
-      throw Error("Could not fetch data");
-    }
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+import { useState, useEffect } from "react";
+import ProductCard from "./Products/Product-Card";
+import styles from "./Main.module.css";
 
 function Main() {
-  return <h1>Main</h1>;
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/catalogue")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setItem(data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ProductCard item={item} />
+    </div>
+  );
 }
 
 export default Main;
