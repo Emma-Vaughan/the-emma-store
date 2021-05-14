73195979765092c1b3e1dbc93991c6551a973ebc
Emma Vaughan <emma.vaughan91@hotmail.com>
2021-05-14 17:44:09 +0100

    Remvoed filter state
    Moved filters over to Main from useFetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,56 +2,21 @@ import { useState, useEffect } from "react";
 
 function useFetch(url) {
   const [catalogue, setCatalogue] = useState([]);
-  const [filters, setFilters] = useState([]);
+  // const [filters, setFilters] = useState([]);
   const [price, setPrice] = useState(false);
   const [sale, setSale] = useState(false);
   const [lessThanTen, setLessThanTen] = useState(false);
 
   function sortByPrice() {
-    const sortedByPrice = [...catalogue].sort((a, b) => {
-      return a.price - b.price;
-    });
-
-    const newPrice = price ? false : true;
-
-    setPrice(newPrice);
-    if (newPrice === true) {
-      setFilters(sortedByPrice);
-    } else {
-      setFilters(catalogue);
-    }
+    setPrice(!price);
   }
 
   function saleItems() {
-    const sortSale = [...catalogue].filter((item) => {
-      return item.price < item.RRP;
-    });
-
-    const onSale = sale ? false : true;
-
-    setSale(onSale);
-
-    if (onSale === true) {
-      setFilters(sortSale);
-    } else {
-      setFilters(catalogue);
-    }
+    setSale(!sale);
   }
 
   function under10() {
-    const filtered = [...catalogue].filter((item) => {
-      return item.price < 1000;
-    });
-
-    const underTen = lessThanTen ? false : true;
-
-    setLessThanTen(underTen);
-
-    if (underTen === true) {
-      setFilters(filtered);
-    } else {
-      setFilters(catalogue);
-    }
+    setLessThanTen(!lessThanTen);
   }
 
   useEffect(() => {
@@ -64,14 +29,21 @@ function useFetch(url) {
       })
       .then((data) => {
         setCatalogue(data);
-        setFilters(data);
       })
       .catch((error) => {
         console.log(error.message);
       });
   }, [url]);
 
-  return { catalogue, filters, sortByPrice, under10, price, saleItems };
+  return {
+    catalogue,
+    sortByPrice,
+    under10,
+    price,
+    saleItems,
+    sale,
+    lessThanTen,
+  };
 }
 
 export default useFetch;
